e73d9a7d570f27d8357040b3eba2bbefd813a5a6
Michael van Genderen <mikevg466@gmail.com>
2017-06-04 21:56:15 -0400

    fixed route tests

--- test/routes.js
+++ test/routes.js
@@ -5,57 +5,48 @@ var supertest = require('supertest');
 var agent = supertest.agent(require('../server/app'));
 
 describe('Routes', () => {
+  const pageOne = {
+    url: 'http://www.test/url.com',
+    image: 'http://www.test/image.com',
+    terms: ['one', 'two'],
+    term_rank: .50,
+    page_rank: .60,
+  };
+  const pageTwo = {
+    url: 'http://www.test2/url.com',
+    image: 'http://www.test2/image.com',
+    terms: ['three', 'two'],
+    term_rank: .50,
+    page_rank: .60,
+  };
+  const pageThree = {
+    url: 'http://www.test3/url.com',
+    image: 'http://www.test3/image.com',
+    terms: ['one', 'two'],
+    term_rank: .50,
+    page_rank: .60,
+  };
+  const pageFour = {
+    url: 'http://www.test4/url.com',
+    image: 'http://www.test4/image.com',
+    terms: ['three', 'one'],
+    term_rank: .50,
+    page_rank: .60,
+  };
   before(() => {
     return db.sync({force: true})
-      .then()
-      .then()
-      .then()
-      .then();
+      .then(() => Page.create(pageOne))
+      .then(() => Page.create(pageTwo))
+      .then(() => Page.create(pageThree))
+      .then(() => Page.create(pageFour));
+  });
+  after(() => {
+    return db.sync({force:true});
   });
   describe('api/pages', () => {
-    const pageOne = {
-      url: 'http://www.test/url.com',
-      image: 'http://www.test/image.com',
-      terms: ['one', 'two'],
-      term_rank: .50,
-      page_rank: .60,
-    };
-    const pageTwo = {
-      url: 'http://www.test2/url.com',
-      image: 'http://www.test2/image.com',
-      terms: ['three', 'two'],
-      term_rank: .50,
-      page_rank: .60,
-    };
-    const pageThree = {
-      url: 'http://www.test3/url.com',
-      image: 'http://www.test3/image.com',
-      terms: ['one', 'two'],
-      term_rank: .50,
-      page_rank: .60,
-    };
-    const pageFour = {
-      url: 'http://www.test4/url.com',
-      image: 'http://www.test4/image.com',
-      terms: ['three', 'one'],
-      term_rank: .50,
-      page_rank: .60,
-    };
-    before(() => {
-      return Page.create(pageOne)
-        .then(() => {
-          return Page.create(pageTwo);
-        })
-        .then(() => {
-          return Page.create(pageThree);
-        })
-        .then(() => {
-          return Page.create(pageFour);
-        });
-    });
-    describe('root', (done) => {
-      it('GET method returns all pages', () => {
-        agent.get('http://localhost:1337/api/pages/')
+    describe('root', () => {
+      it('GET method returns all pages', done => {
+        agent.get('/api/pages/')
           .expect(200)
           .end(function (err, res) {
             if (err) return done(err);
@@ -64,8 +55,8 @@ describe('Routes', () => {
             done();
           });
       });
-      xit('query string filter returns pages associated with a specfic term', (done) => {
-        agent.get('http://localhost:1337/api/pages?terms=one')
+      it('query string filter returns pages associated with a specfic term', done => {
+        agent.get('/api/pages?terms=one')
           .expect(200)
           .end(function (err, res) {
             if (err) return done(err);
@@ -76,12 +67,12 @@ describe('Routes', () => {
             ];
             expect(res.body).to.be.an('array');
             expect(res.body).to.have.a.lengthOf(3);
-            expect(res.body).to.deep.equal(testArr);
+            expect(res.body.map(page => page.url)).to.deep.equal(testArr.map(page => page.url));
             done();
           });
       });
-      xit('query string filters for multiple terms space separated', (done) => {
-        agent.get('http://localhost:1337/api/pages?terms=one%20two')
+      it('query string filters for multiple terms space separated', done => {
+        agent.get('/api/pages?terms=one%20two')
           .expect(200)
           .end(function (err, res) {
             if (err) return done(err);
@@ -91,10 +82,10 @@ describe('Routes', () => {
             ];
             expect(res.body).to.be.an('array');
             expect(res.body).to.have.a.lengthOf(2);
-            expect(res.body).to.deep.equal(testArr);
+            expect(res.body.map(page => page.url)).to.deep.equal(testArr.map(page => page.url));
             done();
           });
-      })
+      });
     });
   });
 });
