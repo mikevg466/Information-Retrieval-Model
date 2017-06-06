3fe13a5f7eb26f7643b69b932066e50d4b4115d4
Mike van Genderen <mikevg466>
2017-06-06 10:16:51 -0400

    feature: adds connection for Page and Query through TermRank table

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -4,6 +4,9 @@ const Page = require('./page');
 const Query = require('./query');
 const TermRank = require('./term_rank');
 
+Page.belongsToMany(Query, {through: TermRank});
+Query.belongsToMany(Page, {through: TermRank});
+
 module.exports = {
   Page,
   Query,
