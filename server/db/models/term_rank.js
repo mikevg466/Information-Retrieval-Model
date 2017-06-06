05c57ce1e348d0d22d5eb32965db6b827f68c622
Mike van Genderen <mikevg466>
2017-06-06 11:05:56 -0400

    feature: adds term_rank instance and class methods

--- server/db/models/term_rank.js
+++ server/db/models/term_rank.js
@@ -2,6 +2,8 @@
 
 const db = require('../db');
 const DataTypes = db.Sequelize;
+const Query = require('./query');
+const Page = require('./page');
 
 module.exports = db.define('term_rank', {
 
@@ -9,4 +11,33 @@ module.exports = db.define('term_rank', {
     type: DataTypes.DOUBLE(),
     defaultValue: 0.00,
   },
+},{
+  instanceMethods: {
+    setRank: function(query){
+      return Page.findById(this.pageId)
+        .then(page => {
+          let totalRank = 0.00;
+          query.terms.forEach(term => {
+            if(page.terms.includes(term)) totalRank++;
+          })
+          console.log(totalRank);
+          this.rank = totalRank;
+          console.log(this.rank);
+        })
+        .catch(console.error.bind(console));
+    }
+  },
+  classMethods: {
+    setRankFromQuery: function(query){
+      return TermRank.findAll({
+        where: {
+          queryId: query.id
+        }
+      })
+        .then(term_rank => {
+          // TODO: add in call to setRank instance method
+        })
+        .catch(console.error.bind(console));
+    }
+  }
 });
